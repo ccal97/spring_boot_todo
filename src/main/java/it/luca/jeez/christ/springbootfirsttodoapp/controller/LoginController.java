500c95c2fc29e7bb802a9fb4643b497fc8efb0c3
ldarech <johndoe@example.com>
2021-06-29 17:04:16 +0200

    quasi jsp. vedi drive doc

--- src/main/java/it/luca/jeez/christ/springbootfirsttodoapp/controller/LoginController.java
+++ src/main/java/it/luca/jeez/christ/springbootfirsttodoapp/controller/LoginController.java
@@ -2,6 +2,7 @@ package it.luca.jeez.christ.springbootfirsttodoapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @Post mapping a seconda delle chiamate che vogliono servire
  *  
  * [ctrl+1 apre le Eclipse suggestion]
+ * 
+ * Per passare dei parametri attraverso alla richiesta
+ * utilizziamo @RequestParam da iniettare nel metodo del controller.
+ * In Spring MVC per passare dati dal Controller al View utilizziamo il Model ancora da inserire
  * @author ldarech
  *
  */
@@ -23,7 +28,8 @@ public class LoginController {
 	
 	@GetMapping(path="/uno")
 	public String loginMessage() {
-		return "Hello world";
+		return "login";	//ritorniamo la view login.jsp. bisogna configurare in app.properties prefix e suffix
+		//però non funziona
 	}
 
 }
